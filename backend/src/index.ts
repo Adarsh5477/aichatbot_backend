b2bdd67bc6980af208fc553f810315161c55a2b6
Adarsh Kumar Dash <adarshdash101@gmail.com>
2024-08-12 19:14:18 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,9 +5,9 @@ import { connectToDatabase } from "./db/connection.js";
 const PORT = process.env.PORT || 5000;
 connectToDatabase()
   .then(() => {
-    app.listen(PORT, () =>
-      console.log("Server Open & Connected To Database 🤟")
-      console.log("AUTH Service is Online")
-    );
+     app.listen(PORT,'0.0.0.0', () => {
+        console.log("Server Open & Connected To Database 🤟")
+        console.log("AUTH Service is Online")
+    });
   })
   .catch((err) => console.log(err));
